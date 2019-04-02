63994a935432c671fbfd660202d72760088c94a5
akhmad0710 <akhmad.0710@students.amikom.ac.id>
2019-04-02 14:54:04 +0700

    latihan construktor

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prak4
+{
+    class Program
+    {
+        static void CetakHasil(string hasil)
+        {
+            Console.WriteLine(hasil);
+        }
+
+        static void Main(string[] args)
+        {
+            //deklarasi variabel
+
+            int hasil;
+
+            //membuat objek dari class kalkulator
+            Kalkulator kalkulator = new Kalkulator();
+
+            //panggil method penjumlahan
+
+            hasil = kalkulator.Penjumlahan(10, 2);
+            CetakHasil("hasil penjumlahan  :" + hasil);
+
+            //panggil method penjumlhan dengan 3 parameter (overload  method)
+            hasil = kalkulator.Penjumlahan(10, 2, 3);
+            CetakHasil("hasil penjumlahan overload : " + hasil);
+
+            //panggil methode prngurangan 
+            hasil = kalkulator.Pengurangan(7, 2);
+            CetakHasil("hasil  pengurangan: " + hasil);
+
+            //panggil static method . langsung dari nama classnya
+            hasil = Kalkulator.Perkalian(5, 2);
+            CetakHasil("hasil perkalian: " + hasil);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ kalkulator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prak4
+{
+    public class Kalkulator
+    {
+        public int Penjumlahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Penjumlahan(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        //overload method
+
+        public int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        //method static
+
+        public static int Perkalian (int a, int b)
+        {
+            return a * b;
+        
+        }
+
+    }
+
+   
+
+}
